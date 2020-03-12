f7a91883e34158ae90058e05712ef2f4f235712f
Some Nath Kundu <dts.somnathK@outlook.com>
2020-03-12 11:48:13 +0530

    added menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,6 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -49,6 +48,9 @@
                                 </li>
                             @endif
                         @else
+                            <li class="{{ Request::is('entries*') ? 'active' : '' }} nav-item">
+                                <a class="nav-link" href="{{ route('entries.index') }}"><i class="fa fa-edit"></i><span>Entries</span></a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
